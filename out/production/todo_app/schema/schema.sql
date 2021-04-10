faeb588fa694a78b77fa18ac0c104838fdbd62e9
Joffrey <45638283+MamaRussia@users.noreply.github.com>
2021-04-10 16:17:07 -0700

    tested working 1st 4 commands

---
+++ out/production/todo_app/schema/schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS todo_db;
+CREATE DATABASE todo_db;
+USE todo_db;
+
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+user_id int NOT NULL AUTO_INCREMENT,
+first_name char(50) NOT NULL,
+last_name char(50) NOT NULL ,
+email_address char(50) NOT NULL,
+phone_number char(10) NOT NULL,
+PRIMARY KEY(user_id)
+);
+
+DROP TABLE IF EXISTS status;
+CREATE TABLE status(
+status_id int NOT NULL AUTO_INCREMENT,
+status char(50) NOT NULL,
+status_date datetime NOT NULL DEFAULT(current_timestamp),
+PRIMARY KEY(status_id)
+);
+
+DROP TABLE IF EXISTS todos;
+CREATE TABLE todos(
+todo_id int NOT NULL AUTO_INCREMENT,
+todo_content char(255) not null,
+created_date datetime NOT NULL DEFAULT(current_timestamp),
+user_id int NOT NULL,
+status_id int NOT NULL,
+PRIMARY KEY(todo_id),
+FOREIGN KEY(user_id) REFERENCES users(user_id),
+FOREIGN KEY(status_id) REFERENCES status(status_id)
+);
